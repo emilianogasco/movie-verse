b4c36932d08f5fde0881e3af225f20d2d24a7a7b
danadiaz <danarodiaz3@gmail.com>
2023-08-07 20:17:47 -0300

    agrego cambios en el register.js

--- app/register.js
+++ app/register.js
@@ -7,6 +7,10 @@ const userPassword = document.getElementById('password').value.toLowerCase();
 const userRepeatPassword = document.getElementById('repeatpassword').value;
 const conditions = document.getElementById('form-check-input').checked;
 
+const PasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 if (userName.length < 3) {
     alert('debe ingresar un nombre valido');
     return;
@@ -17,6 +21,35 @@ if (userLastName.length < 3) {
     return;
 }
 
+if (!emailRegex.test(userEmail)) {
+    alert('Debe ingresar un email valido');
+    return;
+}
 
+if (!PasswordRegex.test(userPassword)) {
+    alert('Debe ingresar un password valido');
+    return;
+}
+
+if (userPassword !== userRepeatPassword) {
+    alert('Las contraseñas no coinciden');
+    return;
+}
+
+if (!conditions) {
+    alert('Debe aceptar los terminos y condiciones');
+    return;
+}
+
+const newUser = {
+name: userName,
+lastName: userLastName,
+password: userPassword,
+email: userEmail,
+};
+
+localStorage.setItem('user', JSON.stringify(newUser));
+
+window.location.href = 'http...'
 
 };
