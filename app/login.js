07ee93881afdf1eed8eecdba59691bd7c303ec9a
leandrocudmani <leandro.cudmani.tucu@gmail.com>
2023-08-09 17:32:31 -0300

    se modificó login html y login js. Tambien se creo el style de login.css

--- app/login.js
+++ app/login.js
@@ -15,3 +15,24 @@ const showPassword = () => {
     closeIcon.classList.add('d-none');
 }
 };
+
+
+const authUser = (event) => {
+ const userEmail = document.getElementById('userEmail').value;
+ const userPass = document.getElementById('userPass').value;
+
+ const storedUsers = JSON.parse(localStorage.getItem('users'));
+
+ const authenticatesUser = storedUsers.find((user) => {
+    if (newUser.email === userEmail && newUserUser.password === userPass) {
+        return user
+    } 
+})
+    if (authenticatesUser) {
+      localStorage.setItem('authUser', JSON.stringify('authenticatesUser'))
+      window.location.href = 'http://127.0.0.1:5501/index.html';
+    } else { 
+        alert('Las credenciales no son correctas');
+    }
+
+};
