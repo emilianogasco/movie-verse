f4213fc63482c106df31779f44ad5c4efbbf441c
Naty <natyaguirre24@gmail.com>
2023-08-15 20:51:01 -0300

    agregue y realize el archivo js

---
+++ app/contact.js
@@ -0,0 +1,35 @@
+const createUser = (event) => {
+    event.preventDefault();
+    const userName = document.getElementById('name').value.toLowerCase();
+    const userEmail = document.getElementById('email').value;
+    const userTextarea = document.getElementById('Textarea').value.toLowerCase();
+
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (userName.length < 3) {
+        alert('Debe ingresar un nombre valido');
+        return;
+    }
+
+    if (!emailRegex.test(userEmail )) {
+        alert('Debe ingresar un email valido');
+        return;
+    }
+
+    
+    if (userTextarea.length < 10) {
+        alert('Debe ingresar mas de 10 letras');
+        return;
+    }
+
+    const newUser = {
+     name: userName,
+     email: userEmail,
+     textarea: userTextarea,
+    };
+
+    localStorage.setItem('user', JSON.stringify(newUser));
+
+    window.location.href ='http://127.0.0.1:5501/index.html';
+
+};
