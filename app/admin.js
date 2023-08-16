65d2ed867ed93f55b2298d688ac9cf9e6f0aebaf
EmilianoGasco <Emilianogasco@hotmail.com>
2023-08-16 01:24:43 -0300

    agregue el boton editar, modifique la mayoria del js, controle todo

--- app/admin.js
+++ app/admin.js
@@ -88,14 +88,42 @@ function bdd() {
   localStorage.setItem('PyS', JSON.stringify(bdpys))
 }
 
+// variable editar
+let edit = false;
+
+//posision de la pelicula
+let posisionPyS;
+
+// formulario de peliculas y series
+let formulario = document.getElementById('form-pys');
+
+//openModal
+let openModal = document.getElementById('abrir-modal');
+
+//Boton agregar
+const btnAgregar = document.getElementById('agregar');
+
+//Boton Cerrar
+const btnCerrar =  document.getElementById('cerrar-modal');
+
+// selector de mensaje
+let mensaje = document.querySelector('.mensaje');
+
 //tranformo la bd en un JSON y lo almacena en la varible por las dudas si no existe crea un array vacio
 const storedPyS = JSON.parse(localStorage.getItem ('PyS')) || [];
 
+// obtengo el id mas grande que tengo en mi Array de P y S y le sumo uno, apply() es parara aplicar la función a un conjunto de valores  del arreglo
+var idMax = Math.max.apply(Math, storedPyS.map(id => {return id.id;}))+1
+
 //obtengo el id tabla donde se mostraran las peliculas y serias
 const tabla = document.getElementById('tabla');
+
 //funcion para obtener la tabla de PyS
 function obtenerTablaPyS(){
+  // limpio la tabla
   tabla.innerHTML=''
+
+  // recorro el array de peliculas y series
   storedPyS.forEach((pys) => {
     const id = pys.id;
     const img = pys.img;
@@ -107,7 +135,7 @@ function obtenerTablaPyS(){
     tabla.innerHTML+=`
       <tr>
         <th scope="row">${id}</th>
-        <td><img src="${img}" alt="" class="img-pys"></td>
+        <td><img src="${img}" alt="${titulo}" class="img-pys"></td>
         <td>${titulo}</td>
         <td>${tipo}</td>
         <td>${categoria}</td>
@@ -124,49 +152,91 @@ function obtenerTablaPyS(){
       </tr>
     
     `
-      
   });
 }
+
 //muestro la tabla
 obtenerTablaPyS();
 
 //editar peliculas y series
 function editar(id) {
- 
-  console.log("Editando elemento con ID:", id);
+  edit = true;
+  
+  //abrimos el modal
+  openModal.click();
+
+  //obtenemos la posicion de la PyS que queremos editar
+  posisionPyS = storedPyS.findIndex(pys => pys.id === id);
+
+  // cargamos los datos en los imput para editar  
+  document.getElementById('titulo').value = storedPyS[posisionPyS].titulo;
+  document.getElementById('tipo').value = storedPyS[posisionPyS].tipo;
+  document.getElementById('categoria').value = storedPyS[posisionPyS].categoria;
+  document.getElementById('descripcion').value = storedPyS[posisionPyS].descripcion;
+  
+  // pregunto si publicado es checked y lo tranformo en un booleano 
+  if(storedPyS[posisionPyS].publicado == 'checked'){
+    const checkeado = storedPyS[posisionPyS].publicado = true;
+    document.getElementById('publicado').checked = checkeado;
+  }
+
+  //modifico el texto del btn agregar por actualizar
+  btnAgregar.textContent='Actualizar';
 }
 
 //eliminar peliculas y series
 function eliminar(id) {
   // aqui con la propiedad findIndex selecciono la posicion en la que se encuentra dentro del array
   const posisionPyS = storedPyS.findIndex(pys => pys.id === id);
-  //preugnto si quiero eliminar la pelicula o serie
+
+  //pregunto si quiero eliminar la pelicula o serie
   if(confirm('¿Quieres relamente eliminar la pelicula o serie?')){
+
     //pregunto si esto no es diferente a -1
     if (posisionPyS !== -1) {
+
       //elimino todo lo de esa posicion
       storedPyS.splice(posisionPyS, 1);
+
       //alamceno la nueva informacion en la key PyS
       localStorage.setItem('PyS', JSON.stringify(storedPyS));
+
+      // obtengo la tabla de peliculas y series
       obtenerTablaPyS();
     }
   }
 }
 
-// selector de mensaje
-let mensaje = document.querySelector('.mensaje');
-
 // funcion para borrar el error
 function cleanError() {
+  // borro el contenido de mensaje
   mensaje.innerHTML='';
 }
-// funcion para borrar agregado
+
+// funcion para borrar el mensaje
 function cleanSuccess() {
+  // borro el contenido de mensaje
   mensaje.innerHTML='';
-  document.getElementById('form-pys').reset();
-  document.getElementById('cerrar-modal').click();
+
+  // reseteo el formulario
+  formulario.reset();
+
+  // hago un click en el btn cerrar
+  btnCerrar.click();
+
+  // limpio la tabla
   tabla.innerHTML='';
-  obtenerTablaPyS()
+
+  // obtengo la tabla de peliculas y series
+  obtenerTablaPyS();
+}
+
+// funcion para resetear el formulario
+function cleanForm(){
+  // reseteo el formularo
+  formulario.reset();
+  // edita pasa a ser falso
+  edit = false
 }
 
 // funcion submit del formulario
@@ -174,49 +244,86 @@ const createPyS = (event) =>{
  
   // evitar el refresco de la pagina
   event.preventDefault();
+  // pregunto si edit es true o false para ver si estoy editando o no
+  if(edit == false){
+    // objeto de peliculas y series
+    let newPyS = {
+      id: idMax,
+      img: "../src/img/pelis/sin-imagen.png",
+      titulo :  document.getElementById('titulo').value,
+      tipo : document.getElementById('tipo').value,
+      categoria : document.getElementById('categoria').value,
+      descripcion : document.getElementById('descripcion').value,
+      publicado : document.getElementById('publicado').checked,
+    };
 
-  // objeto de peliculas y series
-  let newPyS = {
-    id: idPyS = storedPyS.length,
-    img: "../src/img/pelis/sin-imagen.png",
-    titulo :  document.getElementById('titulo').value,
-    tipo : document.getElementById('tipo').value,
-    categoria : document.getElementById('categoria').value,
-    descripcion : document.getElementById('descripcion').value,
-    publicado : document.getElementById('publicado').checked,
-  };
+    // consulta si selecciono algun tipo de pelicula o serie
+    if(newPyS.tipo == 0){
+      mensaje.innerHTML= `<div class='alert alert-danger mt-3' role='alert'>Debe seleccionar si es pelicula o serie</div>`
+      setTimeout(cleanError,3000);
+      return;
+    }
 
-  // consulta si selecciono algun tipo de pelicula o serie
-  if(newPyS.tipo == 0){
-    mensaje.innerHTML= `<div class='alert alert-danger mt-3' role='alert'>Debe seleccionar si es pelicula o serie</div>`
-    setTimeout(cleanError,3000);
-    return;
+    //consulta si selecciono alguna categoria
+    if(newPyS.categoria == 0){
+      mensaje.innerHTML= `<div class='alert alert-danger mt-3' role='alert'>Debe seleccionar una categoria</div>`
+      setTimeout(cleanError,3000);
+      return;
+    }
+
+    //cambio el valor de true por checked
+    if(newPyS.publicado == true){
+      newPyS.publicado = 'checked'
+    }
+    
+    //crea una variable que equivale al storedPyS
+    const PyS = storedPyS;
+    // // agregar al JSON el objeto de peliculas y serie
+    PyS.push(newPyS)
+
+    // agregar al JSON el objeto de peliculas y serie
+    localStorage.setItem('PyS',JSON.stringify(PyS));
+
+    idMax = idMax+1;
+    // // muestra un mensaje que dice que se agrego la plelicula
+    mensaje.innerHTML=  `<div class='alert alert-success mt-3' role='alert'>Se agrego correctamente</div>`
+    
+    setTimeout(cleanSuccess, 2000);
+  }else{
+    // creo el objeto con las nuevas modificaciones
+    let upDatePyS = {
+      id: storedPyS[posisionPyS].id,
+      img : storedPyS[posisionPyS].img,
+      titulo :  document.getElementById('titulo').value,
+      tipo : document.getElementById('tipo').value,
+      categoria : document.getElementById('categoria').value,
+      descripcion : document.getElementById('descripcion').value,
+      publicado : document.getElementById('publicado').checked,
+    };
+
+    // cambio el valor de true por checked
+    if(upDatePyS.publicado == true){
+      upDatePyS.publicado = 'checked'
+    }
+
+    // edit pasa a ser falso ahora
+    edit = false
+    
+    // elimino 1 objeto en la posicion y cargo el nuevo valor en la misma posicion (reemplazo)
+    storedPyS.splice(posisionPyS,1,upDatePyS)
+    
+    // introduzco los datos nuevos actualizados
+    localStorage.setItem('PyS', JSON.stringify(storedPyS));
+    
+    // muestro un mensaje 
+    mensaje.innerHTML=  `<div class='alert alert-success mt-3' role='alert'>Se actualizo correctamente</div>`
+    
+    // ejecuto la funcion 
+    setTimeout(cleanSuccess, 2000);
+    
+    // al btn agregar le modifico el texto
+    btnAgregar.innerText = 'Agregar';
   }
-
-  //consulta si selecciono alguna categoria
-  if(newPyS.categoria == 0){
-    mensaje.innerHTML= `<div class='alert alert-danger mt-3' role='alert'>Debe seleccionar una categoria</div>`
-    setTimeout(cleanError,3000);
-    return;
-  }
-
-  //cambio el valor de true por checked
-  if(newPyS.publicado == true){
-    newPyS.publicado = 'checked'
-  }
-
-  //crea una variable que equivale al storedPyS
-   const PyS = storedPyS;
-  // // agregar al JSON el objeto de peliculas y serie
-   PyS.push(newPyS)
-
-  // agregar al JSON el objeto de peliculas y serie
-  localStorage.setItem('PyS',JSON.stringify(PyS))
-  
-  // // muestra un mensaje que dice que se agrego la clave
-  mensaje.innerHTML=  `<div class='alert alert-success mt-3' role='alert'>Se agrego correctamente</div>`
-
-  setTimeout(cleanSuccess, 2000);
 }
 
 
